f79a3c327ff4236797fffa9438e3d7126bf46abc
Moncef Gaha <moncefplastin07@gmail.com>
2020-11-13 07:13:18 +0100

    add sun icon as svg icon

---
+++ components/SunIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'https://esm.sh/react'
+
+export default function SunIcon({ width = 75 }: { width?: number }) {
+    return (
+        <img src="/sun.svg" width={width} title="Sun Icon" />
+    )
+}
