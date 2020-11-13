37161e4c9b917f749d59c2dbd62476b10750a8c1
Moncef Gaha <moncefplastin07@gmail.com>
2020-11-13 07:39:31 +0100

    just change Light mode icon title

--- components/SunIcon.tsx
+++ components/SunIcon.tsx
@@ -2,6 +2,6 @@ import React from 'https://esm.sh/react'
 
 export default function SunIcon({ width = 75 }: { width?: number }) {
     return (
-        <img src="/sun.svg" width={width} title="Sun Icon" />
+        <img src="/sun.svg" width={width} title="Light mode" />
     )
 }
