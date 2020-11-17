1c93feef14c07b7559eebada251ad43cfbf69c5f
Moncef Gaha <moncefplastin07@gmail.com>
2020-11-17 14:54:44 +0100

    add seo meta tags

--- app.tsx
+++ app.tsx
@@ -5,7 +5,17 @@ export default function App({ Page, pageProps }: { Page: ComponentType<any>, pag
     return (
         <>
             <Head>
+                <meta charSet="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <title>Moncef Gaha</title>
+                <meta name="theme-color" content="#000"/>
+                <meta name="description" content="Moncef Gaha website, Web backend developer from Algeria/Biskra"/>
+                <meta name="robots" content="index,follow"/>
+                <meta name="googlebot" content="index,follow"/>
+                <meta name="image" content="https://www.gravatar.com/avatar/ea52ed86c481ca2816b8403acdbc9169?size=256"/>
+                <meta name="geo.region" content="Algaria"/>
+                <meta name="geo.placename" content="Biskra"/>
+
                 <link
                     rel="icon"
                     type="image/png"
@@ -18,7 +28,6 @@ export default function App({ Page, pageProps }: { Page: ComponentType<any>, pag
                     sizes="16x16"
                     href="https://www.gravatar.com/avatar/ea52ed86c481ca2816b8403acdbc9169?size=16"
                 />
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
             </Head>
             <Page {...pageProps} />
